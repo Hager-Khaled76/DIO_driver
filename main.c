7f72b6a0210e8608230576fcb5751b7c4a87f6b0
Hager-Khaled76 <157922503+Hager-Khaled76@users.noreply.github.com>
2024-08-22 04:54:48 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,33 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 11, 2024
+ *      Author: !?
+ */
+
+// here hedear file to function
+
+// «Ì—Ê— «· ŸÂ— ·Ì Â‰« »—œÊ ÂÊ function main «·⁄«œÌÂ
+#include <util/delay.h>
+#include "DIO_int.h"
+
+int main ()
+{
+
+	while(1)
+	{
+		DIO_enumSetPinDirection(DIO_u8PORTA,DIO_u8PIN0,OUTPUT);
+		DIO_enumSetPinValue(DIO_u8PORTA,DIO_u8PIN0, HIGH);
+		_delay_ms(100);
+		DIO_enumTogPinValue(DIO_u8PORTA , DIO_u8PIN0 );
+	    //DIO_enumTogPortValue(DIO_u8PORTA );
+	    _delay_ms(100);
+
+
+	}
+
+
+
+return 0;
+
+}
